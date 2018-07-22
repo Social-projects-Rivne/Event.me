da28d048f6315f44f31afbc095ee1542c925072c
oleksandrtarar <tarar4ik@gmail.com>
2018-07-22 11:54:08 +0300

    Add config for seeding

--- server/config.py
+++ server/config.py
@@ -1,3 +1,69 @@
-import os
+import ConfigParser
+import subprocess
 
-SQLALCHEMY_DATABASE = 'postgresql://admin:1@localhost:9000/eventme'
+config = ConfigParser.RawConfigParser()
+config.read('config.cfg')
+
+"""
+    Migration
+"""
+
+alembic = config.get('alembic', 'migration')
+
+run = subprocess.Popen(alembic.split(), stdout=subprocess.PIPE)
+
+print run.communicate()
+
+"""
+    Seeding
+"""
+
+# seed_roles #
+
+seed_roles = config.get('seeding', 'seed_roles')
+run = subprocess.Popen(seed_roles.split(), stdout=subprocess.PIPE)
+print run.communicate()
+
+seed_tags = config.get('seeding', 'seed_tags')
+run = subprocess.Popen(seed_tags.split(), stdout=subprocess.PIPE)
+print run.communicate()
+
+seed_categories = config.get('seeding', 'seed_categories')
+run = subprocess.Popen(seed_categories.split(), stdout=subprocess.PIPE)
+print run.communicate()
+
+seed_user_statuses = config.get('seeding', 'seed_user_statuses')
+run = subprocess.Popen(seed_user_statuses.split(), stdout=subprocess.PIPE)
+print run.communicate()
+
+seed_event_statuses = config.get('seeding', 'seed_event_statuses')
+run = subprocess.Popen(seed_event_statuses.split(), stdout=subprocess.PIPE)
+print run.communicate()
+
+seed_users = config.get('seeding', 'seed_users')
+run = subprocess.Popen(seed_users.split(), stdout=subprocess.PIPE)
+print run.communicate()
+
+seed_events = config.get('seeding', 'seed_events')
+run = subprocess.Popen(seed_events.split(), stdout=subprocess.PIPE)
+print run.communicate()
+
+seed_event_tags = config.get('seeding', 'seed_event_tags')
+run = subprocess.Popen(seed_event_tags.split(), stdout=subprocess.PIPE)
+print run.communicate()
+
+seed_event_history = config.get('seeding', 'seed_event_history')
+run = subprocess.Popen(seed_event_history.split(), stdout=subprocess.PIPE)
+print run.communicate()
+
+seed_gallery = config.get('seeding', 'seed_gallery')
+run = subprocess.Popen(seed_gallery.split(), stdout=subprocess.PIPE)
+print run.communicate()
+
+seed_feedback = config.get('seeding', 'seed_feedback')
+run = subprocess.Popen(seed_feedback.split(), stdout=subprocess.PIPE)
+print run.communicate()
+
+seed_subscribes = config.get('seeding', 'seed_subscribes')
+run = subprocess.Popen(seed_subscribes.split(), stdout=subprocess.PIPE)
+print run.communicate()
--- server/seeding/seed_event_tags.py
+++ server/seeding/seed_event_tags.py
@@ -4,9 +4,9 @@ from sqlalchemy import create_engine
 engine = create_engine('postgresql://admin:1@localhost:5555/eventme')
 
 with engine.connect() as con:
-    data = ({"event_id": 1, "tag_id": 8}, {"event_id": 1, "tag_id": 9}, {"event_id": 1, "tag_id": 14},
-            {"event_id": 2, "tag_id": 10}, {"event_id": 2, "tag_id": 12}, {"event_id": 3, "tag_id": 11},
-            {"event_id": 2, "tag_id": 12})
+    data = ({"event_id": 1, "tag_id": 1}, {"event_id": 1, "tag_id": 2}, {"event_id": 1, "tag_id": 7},
+            {"event_id": 2, "tag_id": 3}, {"event_id": 2, "tag_id": 5}, {"event_id": 3, "tag_id": 4},
+            {"event_id": 2, "tag_id": 5})
     
     statement = text("""INSERT INTO event_tags(event_id, tag_id) VALUES (:event_id, :tag_id)""")
     
