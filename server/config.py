8ffe78e001c55bfb5b35583edafad1fddc633828
oleksandrtarar <tarar4ik@gmail.com>
2018-07-23 15:18:33 +0300

    Update seed config

--- server/config.py
+++ server/config.py
@@ -20,35 +20,35 @@ print run.communicate()
 
 # seed_roles #
 
-seed_roles = config.get('seeding', 'seed_roles')
+seed_roles = config.get('seeding', 'seed_role')
 run = subprocess.Popen(seed_roles.split(), stdout=subprocess.PIPE)
 print run.communicate()
 
-seed_tags = config.get('seeding', 'seed_tags')
+seed_tags = config.get('seeding', 'seed_tag')
 run = subprocess.Popen(seed_tags.split(), stdout=subprocess.PIPE)
 print run.communicate()
 
-seed_categories = config.get('seeding', 'seed_categories')
+seed_categories = config.get('seeding', 'seed_category')
 run = subprocess.Popen(seed_categories.split(), stdout=subprocess.PIPE)
 print run.communicate()
 
-seed_user_statuses = config.get('seeding', 'seed_user_statuses')
+seed_user_statuses = config.get('seeding', 'seed_user_status')
 run = subprocess.Popen(seed_user_statuses.split(), stdout=subprocess.PIPE)
 print run.communicate()
 
-seed_event_statuses = config.get('seeding', 'seed_event_statuses')
+seed_event_statuses = config.get('seeding', 'seed_event_status')
 run = subprocess.Popen(seed_event_statuses.split(), stdout=subprocess.PIPE)
 print run.communicate()
 
-seed_users = config.get('seeding', 'seed_users')
+seed_users = config.get('seeding', 'seed_user')
 run = subprocess.Popen(seed_users.split(), stdout=subprocess.PIPE)
 print run.communicate()
 
-seed_events = config.get('seeding', 'seed_events')
+seed_events = config.get('seeding', 'seed_event')
 run = subprocess.Popen(seed_events.split(), stdout=subprocess.PIPE)
 print run.communicate()
 
-seed_event_tags = config.get('seeding', 'seed_event_tags')
+seed_event_tags = config.get('seeding', 'seed_event_tag')
 run = subprocess.Popen(seed_event_tags.split(), stdout=subprocess.PIPE)
 print run.communicate()
 
@@ -64,6 +64,6 @@ seed_feedback = config.get('seeding', 'seed_feedback')
 run = subprocess.Popen(seed_feedback.split(), stdout=subprocess.PIPE)
 print run.communicate()
 
-seed_subscribes = config.get('seeding', 'seed_subscribes')
+seed_subscribes = config.get('seeding', 'seed_subscribe')
 run = subprocess.Popen(seed_subscribes.split(), stdout=subprocess.PIPE)
 print run.communicate()
