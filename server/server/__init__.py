eca4a61e08f22cee3c73f5d46f61efd1ae8ebd2b
Artem Korniichuk <artem17thomas@gmail.com>
2018-07-27 14:08:39 +0300

    Add mailer and funcoin that send on mail

--- server/server/__init__.py
+++ server/server/__init__.py
@@ -5,7 +5,9 @@ def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application."""
     config = Configurator(settings=settings)
     config.include('pyramid_jinja2')
+    config.include('pyramid_mailer')
     config.include('.models')
     config.include('.routes')
     config.scan()
     return config.make_wsgi_app()
+
--- server/server/views/default.py
+++ server/server/views/default.py
@@ -1,6 +1,15 @@
+import transaction
+from pyramid_mailer.mailer import Mailer
+from pyramid_mailer.message import Message
 from pyramid.view import view_config
-
 
 @view_config(route_name='home', renderer='json')
 def my_view(request):
+    mailer = request.mailer
+    message = Message(subject="hello world",
+                      sender="eventmerv@gmail.com",
+                      recipients=["RayMetHD@gmail.com"],
+                      body="hello, arthur")
+    mailer.send(message)
+    mailer.send_immediately(message, fail_silently=False)
     return {'status': 'OK'}
