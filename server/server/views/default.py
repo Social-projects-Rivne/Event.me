161555fec8034c5728b766b682a14cf303f5b07a
Artem Korniychuk <artem17thomas@gmail.com>
2018-07-30 16:51:32 +0300

    Add check email on DB when send request for recover password

--- server/server/views/default.py
+++ server/server/views/default.py
@@ -2,14 +2,19 @@ import transaction
 from pyramid_mailer.mailer import Mailer
 from pyramid_mailer.message import Message
 from pyramid.view import view_config
+from ..models.user import User
 
 @view_config(route_name='home', renderer='json')
 def my_view(request):
-    mailer = request.mailer
-    message = Message(subject="hello world",
-                      sender="eventmerv@gmail.com",
-                      recipients=["RayMetHD@gmail.com"],
-                      body="hello, arthur")
-    mailer.send(message)
-    mailer.send_immediately(message, fail_silently=False)
-    return {'status': 'OK'}
+    jsn = request.json_body
+    user = request.dbsession.query(User).filter_by(email=jsn['email']).one_or_none()
+    if user.email is not None:
+        mailer = request.mailer
+        message = Message(subject="hello world",
+                          sender="eventmerv@gmail.com",
+                          recipients=[jsn["email"]],
+                          body="hello, arthur")
+        mailer.send(message)
+        mailer.send_immediately(message, fail_silently=False)
+
+    return "olala"
