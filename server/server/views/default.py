99e9aec2a108abd1c2159af4bc373a4f51ab15f1
Artem Korniychuk <artem17thomas@gmail.com>
2018-07-31 16:02:21 +0300

    Add recover view for recover password

--- server/server/views/default.py
+++ server/server/views/default.py
@@ -1,20 +1,6 @@
-import transaction
-from pyramid_mailer.mailer import Mailer
-from pyramid_mailer.message import Message
 from pyramid.view import view_config
-from ..models.user import User
+
 
 @view_config(route_name='home', renderer='json')
 def my_view(request):
-    jsn = request.json_body
-    user = request.dbsession.query(User).filter_by(email=jsn['email']).one_or_none()
-    if user.email is not None:
-        mailer = request.mailer
-        message = Message(subject="hello world",
-                          sender="eventmerv@gmail.com",
-                          recipients=[jsn["email"]],
-                          body="hello, arthur")
-        mailer.send(message)
-        mailer.send_immediately(message, fail_silently=False)
-
-    return "olala"
+    return {'status': 'OK'}
---
+++ server/server/views/recover.py
@@ -0,0 +1,34 @@
+import transaction
+from pyramid_mailer.mailer import Mailer
+from pyramid_mailer.message import Message
+from pyramid.view import view_config
+from server.models.user_status import UserStatus
+from passlib.hash import pbkdf2_sha256
+from passlib.totp import generate_secret
+from ..models.user import User
+
+@view_config(route_name='recover_password', renderer='json')
+def my_view(request):
+    jsn = request.json_body
+    user = request.dbsession.query(User).filter_by(email=jsn['email']).one_or_none()
+    if user.email is not None:
+        token = request.dbsession.query(User).filter_by(email=jsn['email']).one_or_none()
+        mailer = request.mailer
+        message = Message(subject="hello world",
+                          sender="eventmerv@gmail.com",
+                          recipients=[jsn["email"]],
+                          body='http://localhost:6543/change_password/')
+        mailer.send(message)
+        mailer.send_immediately(message, fail_silently=False)
+
+    return "olala"
+
+@view_config(route_name='change_password', renderer='json')
+def my_view(request):
+    jsn = request.json_body
+    user = request.dbsession.query(User).filter_by(email=jsn['email']).one_or_none()
+    password = request.dbsession.query(User).filter_by(password=jsn['password']).one_or_none()
+    if user.email is not None:
+        user = request.dbsession.query(User).filter_by(email=jsn['email']).update(dict(password=jsn['password']))
+    
+    return {'status': 'OK'}
