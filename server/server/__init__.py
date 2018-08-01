e46cc5b6d10edc75965df17c40c480e8cd9ed4a5
Andrew Black <andrey8blak@gmail.com>
2018-08-01 13:56:25 +0300

    Turn debug mode off

--- server/server/__init__.py
+++ server/server/__init__.py
@@ -5,7 +5,7 @@ def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application."""
     config = Configurator(settings=settings)
     config.include('pyramid_jinja2')
-    config.include("cornice")
+    config.include('cornice')
     config.include('.models')
     config.include('.security')
     config.scan()
