906eb8507990bda4d7c449b4f39b698f9fa5420d
Andrew Black <andrey8blak@gmail.com>
2018-08-02 14:08:41 +0300

    Implement testing AJAX request

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,6 +1,23 @@
 import React, { Component } from 'react';
 
+let headers = new Headers()
+
+const myInit = {method: 'GET',
+                mode: 'cors',
+                headers: headers,
+                cache: 'default' }
+
 export class Home extends Component {
+  componentDidMount() {
+    fetch('http://localhost:6543', myInit)
+      .then(response => {
+        return response.json()
+      })
+      .then(data => {
+        console.log('Data: ', data)
+      })
+  }
+
   render() {
     return (
         <h1>This is Home</h1>
--- server/server/views/default.py
+++ server/server/views/default.py
@@ -3,4 +3,10 @@ from pyramid.view import view_config
 
 @view_config(route_name='home', renderer='json')
 def my_view(request):
+    request.response.headerlist.extend(
+        (
+            ('Access-Control-Allow-Origin', '*'),
+            ('Content-Type', 'application/json')
+        )
+    )
     return {'status': 'OK'}
