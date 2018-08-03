f9a4ad5a133c24188cb859b6d3dc1bfaed444274
Artem Korniichuk <artem17thomas@gmail.com>
2018-08-03 13:51:34 +0300

    add temporary password, and cornice

--- server/server/views/recover.py
+++ server/server/views/recover.py
@@ -1,4 +1,6 @@
+import random, string
 import transaction
+from cornice.resource import resource, view
 from passlib.totp import generate_secret
 from pyramid_mailer.mailer import Mailer
 from pyramid_mailer.message import Message
@@ -8,28 +10,42 @@ from pyramid.view import view_config
 
 from ..models.user import User
 
+
 @view_config(route_name='recover_password', renderer='json')
+#def password_generator(size=8, chars=string.ascii_letters + string.digits):
+    #return ''.join(random.choice(chars) for i in range(size))
+
 def recover_send_mail(request):
+    global temporary_password
+    random_charset = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    temporary_password = "".join(random.sample(random_charset,8 ))
     json = request.json_body
     user = request.dbsession.query(User).filter_by(email=json['email']).one_or_none()
     if user.email is not None:
         token = request.dbsession.query(User).filter_by(email=json['email']).one_or_none()
         mailer = request.mailer
-        message = Message(subject="hello world",
+        message = Message(subject="Recover password",
                           sender="eventmerv@gmail.com",
                           recipients=[json["email"]],
-                          body='http://localhost:6543/change_password/')
+                          body='temporary password:'+ temporary_password +' http://localhost:6543/change_password/')
         mailer.send(message)
         mailer.send_immediately(message, fail_silently=False)
 
     return "We send link for change password in your mail", json['email']
 
+@resource(path='change_password', renderer='json')
+class Change_Password(object):
+
+    def __init__(self, request, context=None):
+        self.request = request
+        self.context = context
+
 @view_config(route_name='change_password', renderer='json')
 def recover_change_password(request):
     json = request.json_body
-    user = request.dbsession.query(User).filter_by(email=jsn['email']).one_or_none()
+    user = request.dbsession.query(User).filter_by(email=json['email']).one_or_none()
     password = request.dbsession.query(User).filter_by(password=json['password']).one_or_none()
     if user.email is not None:
-        user = request.dbsession.query(User).filter_by(email=json['email']).update(dict(password=json['password']))
+        user = request.dbsession.query(User).filter_by(email=json['email']).update(dict(password=pbkdf2_sha256.hash(json['password'])))
 
     return {'status': 'OK'}
