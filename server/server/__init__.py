4938d724cc7c5fd06358a11f11fbfd82c0142bef
Artem Korniichuk <artem17thomas@gmail.com>
2018-08-03 13:50:25 +0300

    add cornice in init

--- server/server/__init__.py
+++ server/server/__init__.py
@@ -6,6 +6,7 @@ def main(global_config, **settings):
     config = Configurator(settings=settings)
     config.include('pyramid_jinja2')
     config.include('pyramid_mailer')
+    config.include('cornice')
     config.include('.models')
     config.include('.routes')
     config.scan()
--- server/server/routes.py
+++ server/server/routes.py
@@ -2,6 +2,6 @@ def includeme(config):
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
     config.add_route('recover_password', '/recover_password')
-    config.add_route('change_password', '/change_password')
+   # config.add_route('change_password', '/change_password')
     config.add_route('log_in', '/log-in')
     config.add_route('log_out', '/log-out')
