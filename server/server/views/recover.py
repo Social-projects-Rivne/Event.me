aace2c7374ba95f019ff653117f45bfff6f54d7c
Artem Korniychuk <artem17thomas@gmail.com>
2018-08-08 16:44:26 +0300

    Change recover

--- server/server/views/recover.py
+++ server/server/views/recover.py
@@ -5,11 +5,13 @@ from passlib.totp import generate_secret
 from pyramid_mailer.mailer import Mailer
 from pyramid_mailer.message import Message
 from passlib.hash import pbkdf2_sha256
-from passlib.hash import oracle10
+from passlib.totp import generate_secret
 from server.models.user_status import UserStatus
 from pyramid.view import view_config
+from pyramid.httpexceptions import HTTPNotFound
 
 from ..models.user import User
+from server.models.user_status import UserStatus
 
 
 recover_password = Service(name='recover_password', path='/recover-password')
@@ -18,20 +20,17 @@ change_password = Service(name='change_password', path='/change-password/{change
 
 @recover_password.post()
 def recover_send_mail(request):
-    global temporary_password, json
-    random_charset = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    temporary_password = "".join(random.sample(random_charset,8 ))
     json = request.json_body
     user = request.dbsession.query(User).filter_by(email=json['email']).one_or_none()
-    password_mail_hash = oracle10.hash(temporary_password, user=json['email'])
-    if user.email is not None:
-        token = request.dbsession.query(User).filter_by(email=json['email']).one_or_none()
+    if (user.email is not None) and user.is_active(request):
+        url_token_confirmation = generate_secret()
+        user.url_token = url_token_confirmation
         mailer = request.mailer
         message = Message(subject="Recover password",
                           sender="eventmerv@gmail.com",
                           recipients=[json["email"]],
-                          body='temporary password:' + temporary_password + '\n' + request.route_url('change_password',
-                                                                                             change_password_hash=password_mail_hash))
+                          body='Follow the link below' + '\n' + request.route_url('change_password',
+                                                                                  change_password_hash=url_token_confirmation))
         mailer.send_immediately(message, fail_silently=False)
 
     return {
@@ -42,12 +41,10 @@ def recover_send_mail(request):
 
 @change_password.post()
 def recover_change_password(request):
+    json = request.json_body
     user_change_password = request.matchdict['change_password_hash']
     user = request.dbsession.query(User).filter_by(email=json['email']).one_or_none()
-    temporary_mail_password = request.dbsession.query(User).filter_by(password=json['password']).one_or_none()
-    password = request.dbsession.query(User).filter_by(password=json['password']).one_or_none()
-
-    if ((user.email is not None) & (temporary_password==request.json_body['temporary_password'] )):
-        user = request.dbsession.query(User).filter_by(email=json['email']).update(dict(password=pbkdf2_sha256.hash(json['password'])))
-
+    if user.email is not None and user.url_token == user_change_password:
+        user.password=pbkdf2_sha256.hash(json['password'])
+        user.url_token = None
     return {'status': 'OK'}
