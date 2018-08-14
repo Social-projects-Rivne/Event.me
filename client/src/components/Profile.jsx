5b1bf67301697f9bf02d7bc6998be7d04832843b
Yaroslav Zhyvyi <yazhyvyi@gmail.com>
2018-08-14 14:01:00 +0300

    Changed 3 letters

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -83,7 +83,7 @@ export default class Profile extends Component {
                 <Row>
                     <Col s={6}>
                         <Row>
-                            <this.renderAvatarImage />
+                            {this.renderAvatarImage()}
                         </Row>
                     </Col>
                     <Col s={2}>
