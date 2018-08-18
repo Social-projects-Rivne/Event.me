484ee193d868f959abb0295ffc6ebd5e414125ec
Yaroslav Zhyvyi <yazhyvyi@gmail.com>
2018-08-18 20:04:57 +0300

    Fixed indentation issues

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -5,77 +5,85 @@ import { request } from '../utils';
 
 
 class Profile extends Component {
-  state = {
-      user: {},
-      page_id: ""
-  };
 
-  componentDidMount() {
-      this.setState({ page_id: this.props.match.params.profile_id });
-      request('/profile/' + this.props.match.params.profile_id, "GET")
-      .then(json => {
-          this.setState({ user: json });
-          console.log(json);
-      })
-  }
+    state = {
+        user: {},
+        page_id: ""
+    };
 
-  renderAvatarImage = () => {
-    if (this.state.user.avatar === null) {
+    componentDidMount() {
+        this.setState({ page_id: this.props.match.params.profile_id });
+        request('/profile/' + this.props.match.params.profile_id)
+        .then(json => {
+            this.setState({ user: json });
+        })
+    }
+
+    renderAvatarImage = () => {
+        if (this.state.user.avatar === null) {
+            return (
+                <img
+                  src="http://dialpharma.com/media/img/default_profile.png"
+                  alt="Default icon"
+                />
+            );
+        }
+        else {
+            return (
+                <img
+                  src={this.state.user.avatar}
+                  alt="Avatar icon"
+                />
+            );
+        }
+    };
+
+    renderEditButton = () => {
+        if (this.props.match.params.profile_id === sessionStorage['User-id']) {
+            return (
+                <Link
+                  className="waves-effect waves-light btn"
+                  to={"/profile-edit/" + sessionStorage['User-id']}>
+                    Edit
+                </Link>
+            );
+        }
+    };
+
+    render() {
       return (
-        <img src="http://dialpharma.com/media/img/default_profile.png" alt="Default icon" />
-      );
-    }
-    else {
-       return (
-         <img src={this.state.user.avatar} alt="Avatar icon" />
-       );
-    }
-   };
-
-  checkID = () => {
-      if (this.props.match.params.profile_id === sessionStorage['User-id']) {
-        return (
-          <Link className="waves-effect waves-light btn" to={"/profile-edit/" + sessionStorage['User-id']}>
-            Edit
-          </Link>
-        );
-      }
-  };
-
-  render() {
-    return (
-      <div>
         <div>
-          <h1>Profile</h1>
-        </div>
+          <div>
+            <h1>Profile</h1>
+          </div>
           <div id="main">
-          <Row>
+            <Row>
               <Col s={6}>
-                  <Row>
-                      {this.renderAvatarImage()}
-                  </Row>
+                <Row>
+                  {this.renderAvatarImage()}
+                </Row>
               </Col>
               <Col s={3}>
-                  <Row s={2}><h4>Nickname:</h4></Row>
-                  <Row s={2}><h4>First Name: </h4></Row>
-                  <Row s={2}><h4>Last Name: </h4></Row>
-                  <Row s={2}><h4>Location: </h4></Row>
+                <Row s={2}><h4>Nickname:</h4></Row>
+                <Row s={2}><h4>First Name: </h4></Row>
+                <Row s={2}><h4>Last Name: </h4></Row>
+                <Row s={2}><h4>Location: </h4></Row>
               </Col>
               <Col s={3}>
-                  <Row s={2}><h4>{this.state.user.nickname}</h4></Row>
-                  <Row s={2}><h4>{this.state.user.first_name}</h4></Row>
-                  <Row s={2}><h4>{this.state.user.last_name}</h4></Row>
-                  <Row s={2}><h4>{this.state.user.location}</h4></Row>
-                  <Row s={2}>
-                      {this.checkID()}
-                  </Row>
+                <Row s={2}><h4>{this.state.user.nickname}</h4></Row>
+                <Row s={2}><h4>{this.state.user.first_name}</h4></Row>
+                <Row s={2}><h4>{this.state.user.last_name}</h4></Row>
+                <Row s={2}><h4>{this.state.user.location}</h4></Row>
+                <Row s={2}>
+                  {this.renderEditButton()}
+                </Row>
               </Col>
-          </Row>
-          <br />
+            </Row>
+            <br />
+          </div>
         </div>
-      </div>
-    );
-  }
+      );
+   }
 }
 
 export default Profile;
--- client/src/components/ProfileEdit.jsx
+++ client/src/components/ProfileEdit.jsx
@@ -6,93 +6,143 @@ import { Link } from 'react-router-dom'
 
 class ProfileEdit extends Component {
 
-	state = {
-		  user_edit: {},
-      error_nick: "",
-      error_repeated: ""
-	}
+    state = {
+        user_edit: {},
+        error_nick: "",
+        error_repeated: ""
+    }
 
-	componentDidMount() {
-		request('/profile/' + this.props.match.params.profile_id, "GET")
-    .then(json => {
-			this.setState({user_edit: json});
-      console.log(this.state.user_edit)
-      this.setState({
-        user_edit: {
-          f_name: this.state.user_edit.first_name,
-          l_name: this.state.user_edit.last_name,
-          nick: this.state.user_edit.nickname,
-          loc: this.state.user_edit.location,
-          pass: this.state.user_edit.password
-        }
-      })
-		})
-	}
-
-	UpdateClick = (eve) => {
-		eve.preventDefault()
-
-    let nick_error = ""
-    let repeated_error = ""
-
-			if (this.state.new_password === this.state.repeat_password) {
-				const data = {
-					"first_name": this.state.f_name,
-					"last_name": this.state.l_name,
-					"nickname": this.state.nickname,
-					"location": this.state.location,
-					"password": this.state.new_password,
-					"id": this.state.user_edit.id
-				}
-				request('/profile/' + this.props.match.params.profile_id, "PUT", JSON.stringify(data))
-        .then(data => {
-          if (data.msg !== "") {
-            this.setState({ error_nick: data.msg })
-          }
-          else {
-            this.props.history.push('/profile/' + this.props.match.params.profile_id);
-            window.Materialize.toast("Profile Updated", 2500);
-          }
+    componentDidMount() {
+        request('/profile/' + this.props.match.params.profile_id)
+        .then(json => {
+            this.setState({user_edit: json});
+            this.setState({
+                user_edit: {
+                    f_name: this.state.user_edit.first_name,
+                    l_name: this.state.user_edit.last_name,
+                    nick: this.state.user_edit.nickname,
+                    loc: this.state.user_edit.location,
+                    pass: this.state.user_edit.password
+                }
+            })
         })
-			} else {
-        repeated_error = "Incorrect repeated password"
-			}
-      this.setState({ error_repeated: repeated_error })
-	};
+    }
 
-  onChangeHandler = (e) => {
-      let { id } = e.currentTarget
-      this.setState({ [id]: e.currentTarget.value })
-  }
+    UpdateClick = (eve) => {
+        eve.preventDefault()
 
-	render() {
-		return (
-      <div>
-  			<div>
-  				<h1>Profile</h1>
-  			</div>
-  			<Row>
-  				<Input s={6} error="" id="f_name" label="First Name" value={this.state.user_edit.first_name} onChange={this.onChangeHandler} placeholder=" " />
-  				<Input s={6} error="" id="l_name" label="Last Name" validate value={this.state.user_edit.last_name} onChange={this.onChangeHandler} placeholder=" " />
-  				<Input s={12} error={this.state.error_nick} id="nickname" label="Nickname" validate value={this.state.user_edit.nickname} onChange={this.onChangeHandler} placeholder=" " />
-  				<Input s={12} error="" id="location" label="Location" validate value={this.state.user_edit.location} onChange={this.onChangeHandler} placeholder=" " />
-  				<Input s={6} error="" id="new_password" label="New password" type="password" onChange={this.onChangeHandler} />
-  				<Input s={6} error={this.state.error_repeated} id="repeat_password" label="Repeat password" type="password" onChange={this.onChangeHandler} />
-  				<Input id="image" type='file' />
-  			</Row>
-        <Row>
-          <Col s={11}>
-  				<Button waves='light' onClick={this.UpdateClick}>Save</Button>
-          </Col>
-          <Col s={1}>
-          <Link className="waves-effect waves-light btn" to={"/profile/" + sessionStorage['User-id']}>
-            Back
-          </Link>
-        </Col>
-        </Row>
-	    </div>
-    );
-	};
+        let msg_rep = ""
+
+        this.setState({ error_repeated: ""})
+
+        if (this.state.new_password === this.state.repeat_password) {
+            const data = {
+                "first_name": this.state.f_name,
+                "last_name": this.state.l_name,
+                "nickname": this.state.nickname,
+                "location": this.state.location,
+                "password": this.state.new_password,
+                "id": this.state.user_edit.id
+            }
+            request('/profile/' + this.props.match.params.profile_id, "PUT", JSON.stringify(data))
+            .then(data => {
+                if (data.msg !== "") {
+                    this.setState({ error_nick: data.msg })
+                }
+                else {
+                    this.setState({ error_nick: "" })
+                    this.props.history.push('/profile/' + this.props.match.params.profile_id);
+                    window.Materialize.toast("Profile Updated", 2500);
+                }
+            })
+        }
+        else {
+            msg_rep = "Incorrect repeated password"
+        }
+        this.setState({ error_repeated: msg_rep })
+    };
+
+    onChangeHandler = (e) => {
+        let { id } = e.currentTarget
+        this.setState({ [id]: e.currentTarget.value })
+    }
+
+    render() {
+      return (
+        <div>
+          <div>
+             <h1>Profile</h1>
+          </div>
+          <Row>
+            <Input
+              s={6}
+              error=""
+              id="f_name"
+              label="First Name"
+              value={this.state.user_edit.first_name}
+              onChange={this.onChangeHandler}
+              placeholder=" "
+            />
+            <Input
+              s={6}
+              error=""
+              id="l_name"
+              label="Last Name"
+              validate value={this.state.user_edit.last_name}
+              onChange={this.onChangeHandler}
+              placeholder=" "
+            />
+            <Input
+              s={12}
+              error={this.state.error_nick}
+              id="nickname"
+              label="Nickname"
+              validate value={this.state.user_edit.nickname}
+              onChange={this.onChangeHandler}
+              placeholder=" "
+            />
+            <Input
+              s={12}
+              error=""
+              id="location"
+              label="Location"
+              validate value={this.state.user_edit.location}
+              onChange={this.onChangeHandler}
+              placeholder=" "
+            />
+            <Input
+              s={6}
+              error=""
+              id="new_password"
+              label="New password"
+              type="password"
+              onChange={this.onChangeHandler}
+            />
+            <Input
+              s={6}
+              error={this.state.error_repeated}
+              id="repeat_password"
+              label="Repeat password"
+              type="password"
+              onChange={this.onChangeHandler}
+            />
+            <Input id="image" type='file' />
+          </Row>
+          <Row>
+            <Col s={11}>
+              <Button waves='light' onClick={this.UpdateClick}>Save</Button>
+            </Col>
+            <Col s={1}>
+              <Link
+                className="waves-effect waves-light btn"
+                to={"/profile/" + sessionStorage['User-id']}>
+                Back
+              </Link>
+            </Col>
+          </Row>
+        </div>
+      );
+    };
 }
 
 export default ProfileEdit;
