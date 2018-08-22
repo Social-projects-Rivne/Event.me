8e5d4f66516776a5c607241a43b0cd3f0cf62e09
asstelite <kobukstr@gmail.com>
2018-08-22 12:40:36 +0300

    fix conflicts

--- client/src/components/Nav/index.jsx
+++ client/src/components/Nav/index.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Navbar, Button } from 'react-materialize';
+import { NavLink } from 'react-router-dom'
 import LogIn from './LogIn';
 import NavProfile from './NavProfile';
 import LogOut from './LogOut';
@@ -7,44 +8,43 @@ import { isLogged } from '../../utils'
 
 
 class Nav extends Component {
-  constructor(props) {
-    super(props);
-    this.update = this.update.bind(this);
-  }
-
-  routeChange(){
-
-      window.location.hash = "/registration";
-
-  }
-
-  update() {
-    this.forceUpdate();
-  };
-
-  renderAuthFileds() {
-    if (isLogged()) {
-      return (
-        <React.Fragment>
-          <NavProfile isLogged={isLogged} />
-          <LogOut update={this.update} />
-        </React.Fragment>
-      )
+    constructor(props) {
+        super(props);
+        this.update = this.update.bind(this);
     }
-    else return (<React.Fragment><LogIn update={this.update} />
-                  <Button onClick={this.routeChange = this.routeChange.bind(this)} waves="light">
-                      Registration
-                  </Button>
-                 </React.Fragment>)
-  }
 
-  render() {
-    return (
-      <Navbar brand="Event.me" right>
-        {this.renderAuthFileds()}
-      </Navbar>
-    );
-  }
+    update() {
+        this.forceUpdate();
+    }
+
+    renderAuthFileds() {
+      if (isLogged()) {
+        return (
+          <React.Fragment>
+            <NavProfile isLogged={isLogged} />
+               <LogOut update={this.update} />
+            </React.Fragment>
+            );
+      }
+        else {
+          return (
+            <React.Fragment>
+               <LogIn update={this.update} />
+               <Button waves="light" node='a' href='registration'>
+                  <NavLink to='registration'>Registration</NavLink>
+               </Button>
+            </React.Fragment>
+            );
+        }
+    }
+
+    render() {
+      return (
+        <Navbar brand="Event.me" right>
+           {this.renderAuthFileds()}
+        </Navbar>
+        );
+    }
 }
 
 export default Nav;
