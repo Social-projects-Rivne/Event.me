a50fdd67913df6c4171ae2d5cf509ad3df6911ab
asstelite <kobukstr@gmail.com>
2018-08-28 13:51:54 +0300

    add the filter by date

--- server/server/views/location_event.py
+++ server/server/views/location_event.py
@@ -1,6 +1,7 @@
 from cornice import Service
-
+from pyramid.response import Response
 from ..models.event import Event
+from ..models.category import Category
 
 
 events_short_info = Service(name='events_short_info',
@@ -8,11 +9,18 @@ events_short_info = Service(name='events_short_info',
                             cors_origins=('http://localhost:3000',))
 
 
-@events_short_info.get()
+@events_short_info.post()
 def get_events_short_info(request):
     """ """
-    responce = {
+    catt = {'holiday': 1,
+            'convert': 2}
+    json = request.json_body
+    response = {
         'info': []
     }
-    responce['info'] = Event.get_events_short_info(request, 14)
-    return responce
+    response['info'] = Event.get_events_short_info(request, int(json['day_filter']))
+    if json['category'] in catt:
+        for i in response['info']:
+            if json['category'] == response['info']['category_id']:
+
+    return response
