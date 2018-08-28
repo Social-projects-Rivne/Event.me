c6d47dea9f63333191c2fe6daaaa73238412d7b2
asstelite <kobukstr@gmail.com>
2018-08-29 02:54:55 +0300

    add the filter by category

--- server/server/views/location_event.py
+++ server/server/views/location_event.py
@@ -1,8 +1,8 @@
 from cornice import Service
 from pyramid.response import Response
 from ..models.event import Event
-from ..models.category import Category
 
+from datetime import datetime, timedelta
 
 events_short_info = Service(name='events_short_info',
                             path='/events-short-info',
@@ -12,15 +12,23 @@ events_short_info = Service(name='events_short_info',
 @events_short_info.post()
 def get_events_short_info(request):
     """ """
-    catt = {'holiday': 1,
-            'convert': 2}
+    datetime_now = datetime.now()
+    categories = {'holiday': 1, 'concert': 2, 'movie': 3, 'Voluntary': 4}
     json = request.json_body
     response = {
         'info': []
     }
-    response['info'] = Event.get_events_short_info(request, int(json['day_filter']))
-    if json['category'] in catt:
-        for i in response['info']:
-            if json['category'] == response['info']['category_id']:
+    if json['category'] in categories:
+        json['category'] = categories[json['category']]
 
-    return response
+    if json['category'] == '':
+        response['info'] = Event.get_events_short_info(request, int(json['day_filter']))
+        return response
+    else:
+        response['info'] = request.dbsession.query(Event).\
+            with_entities(Event.id, Event.long, Event.lat, Event.name, Event.category_id).\
+            filter(Event.start_date > datetime_now,
+                   Event.start_date < datetime_now +
+                   timedelta(days=int(json['day_filter'])),
+                   Event.category_id == json['category']).all()
+        return response
