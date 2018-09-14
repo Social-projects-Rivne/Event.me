e1790db8e33bbdbdb3b10dcca47a861c6978c8d2
asstelite <kobukstr@gmail.com>
2018-09-14 15:29:16 +0300

    add button

--- client/src/components/AdminPage.jsx
+++ client/src/components/AdminPage.jsx
@@ -6,6 +6,7 @@ const AdminPage = () => {
   return (
     <React.Fragment>
       <Link to="/admin-page/users" className="btn waves-effect waves-light btn-flat">Users</Link>
+      <Link to="/admin-page/events" className="btn waves-effect waves-light btn-flat">Events</Link>
     </React.Fragment>
     );
 }
